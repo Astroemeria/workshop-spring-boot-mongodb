ecbce11ee5c3abc2b245d7b242fbdc587c627264
astroemeria <aline9909@gmail.com>
2021-10-04 14:19:50 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/astroemeria/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astroemeria.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
