d481bdec9cce45e03fe1698a03e17db33c19284d
astroemeria <aline9909@gmail.com>
2021-10-05 10:18:58 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/astroemeria/workshopmongo/resources/UserResource.java
+++ src/main/java/com/astroemeria/workshopmongo/resources/UserResource.java
@@ -1,15 +1,17 @@
 package com.astroemeria.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.astroemeria.workshopmongo.domain.User;
+import com.astroemeria.workshopmongo.dto.UserDTO;
 import com.astroemeria.workshopmongo.services.UserService;
 
 @RestController //para dizer que a classe vai ser um recurso rest
@@ -20,11 +22,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	@CrossOrigin(origins = "*")
-	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria", "maria@gmail.com");
-		User alex = new User("2", "Alex Santos", "alex@gmail.com");
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll(); //busca no banco de dados e guarda na lista
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
